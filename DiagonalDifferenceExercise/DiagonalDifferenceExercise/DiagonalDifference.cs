0f45c8d9d0a0d55718326ae5a3c7ede2061179b4
RicardoGoncalves-CS <126096527+RicardoGoncalves-CS@users.noreply.github.com>
2023-05-10 15:01:09 +0100

    DiagonalDifference exercise

---
+++ DiagonalDifferenceExercise/DiagonalDifferenceExercise/DiagonalDifference.cs
@@ -0,0 +1,27 @@
+﻿namespace DiagonalDifferenceExercise;
+
+// Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+public static class DiagonalDifference
+{
+    public static int Calculate(List<List<int>> arr)
+    {
+        int leftRightPosition = 0;
+        int rightLeftPosition = arr.Count - 1;
+        int sumLeftRight = 0;
+        int sumRightLeft = 0;
+
+        foreach (var list in arr)
+        {
+            sumLeftRight += list[leftRightPosition];
+            leftRightPosition++;
+        }
+
+        foreach (var list in arr)
+        {
+            sumRightLeft += list[rightLeftPosition];
+            rightLeftPosition--;
+        }
+
+        return Math.Abs(sumLeftRight - sumRightLeft);
+    }
+}
---
+++ DiagonalDifferenceExercise/DiagonalDifferenceExercise/Program.cs
@@ -0,0 +1,15 @@
+﻿using DiagonalDifferenceExercise;
+
+List<List<int>> matrix = new List<List<int>>
+{
+    new List<int>{ 3, 5, 7, 9 },
+    new List<int>{ 0, 1, 3, 1 },
+    new List<int>{ 6, 1, 7, 9 },
+    new List<int>{ 4, 5, 8, 2 },
+};
+
+// Left-Right diagonal: 3 + 1 + 7 + 2 = 13
+// Right-Left diagonal: 9 + 3 + 1 + 4 = 17
+// | 13 - 17 | = 4
+
+Console.WriteLine(DiagonalDifference.Calculate(matrix));
