f2156ffcae540cbe24d5dc76aaae1fb573550b71
RicardoGoncalves-CS <126096527+RicardoGoncalves-CS@users.noreply.github.com>
2023-05-10 14:37:28 +0100

    TimeConversion exercise

---
+++ TimeConversionExercise/TimeConversionExercise/Program.cs
@@ -0,0 +1,5 @@
+﻿using TimeConversionExercise;
+
+string time = "11:21:47PM";
+
+Console.WriteLine(TimeConversion.Convert(time));
---
+++ TimeConversionExercise/TimeConversionExercise/TimeConversion.cs
@@ -0,0 +1,30 @@
+﻿namespace TimeConversionExercise;
+
+// Given a time in 12-hour AM/PM format, convert it to military (24-hour) time. 
+public static class TimeConversion
+{
+    public static string Convert(string s)
+    {
+        if (s[8] == 'A')
+        {
+            if (s.Substring(0, 2) == "12")
+                return "00" + s.Substring(2, 6);
+            else
+                return s.Substring(0, 8);
+        }
+
+        else
+        {
+            if (s.Substring(0, 2) == "12")
+            {
+                return s.Substring(0, 8);
+            }
+            else
+            {
+                int hours = int.Parse(s.Substring(0, 2));
+                hours += 12;
+                return hours.ToString("00") + s.Substring(2, 6);
+            }
+        }
+    }
+}
