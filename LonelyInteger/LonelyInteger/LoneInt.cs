46df4ecb7e28d585d83fe3d83e9a8b01b270293e
RicardoGoncalves-CS <126096527+RicardoGoncalves-CS@users.noreply.github.com>
2023-05-10 14:09:08 +0100

    LonelyInteger exercise

---
+++ LonelyInteger/LonelyInteger/LoneInt.cs
@@ -0,0 +1,26 @@
+﻿namespace LonelyInteger;
+
+public static class LoneInt
+{
+    // >> Given an array of integers, where all elements but one occur twice, find the unique element. 
+    public static int Find(List<int> a)
+    {
+        Dictionary<int, int> count = new Dictionary<int, int>();
+
+        foreach (var val in a)
+        {
+            if (count.ContainsKey(val))
+                count[val]++;
+            else
+                count[val] = 1;
+        }
+
+        foreach (var kvp in count)
+        {
+            if (kvp.Value == 1)
+                return kvp.Key;
+        }
+
+        return -1;
+    }
+}
---
+++ LonelyInteger/LonelyInteger/Program.cs
@@ -0,0 +1,7 @@
+﻿using LonelyInteger;
+
+List<int> arr = new List<int> { 1, 1, 4, 5, 5, 6, 6 };
+
+int loneInt = LoneInt.Find(arr);
+
+Console.WriteLine(loneInt);
