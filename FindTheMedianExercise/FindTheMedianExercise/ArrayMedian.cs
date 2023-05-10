21fb5cce7b735a871a16f0a1859310b1f0b31d7e
RicardoGoncalves-CS <126096527+RicardoGoncalves-CS@users.noreply.github.com>
2023-05-10 15:46:06 +0100

    FindTheMedian exercise

---
+++ FindTheMedianExercise/FindTheMedianExercise/ArrayMedian.cs
@@ -0,0 +1,15 @@
+﻿namespace FindTheMedianExercise;
+
+// Given a list of integers with an odd number of elements, find the median
+public static class ArrayMedian
+{
+    public static int findMedian(List<int> arr)
+    {
+        arr.Sort();
+
+        int length = arr.Count;
+        int mid = length / 2;
+
+        return arr[mid];
+    }
+}
---
+++ FindTheMedianExercise/FindTheMedianExercise/Program.cs
@@ -0,0 +1,5 @@
+﻿using FindTheMedianExercise;
+
+List<int> arr = new List<int> { 5, 6, 1, 8, 3, 2, 7 };
+
+Console.WriteLine(ArrayMedian.findMedian(arr));
