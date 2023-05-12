36946dae8a351cb9a824cf858eb684df6556e295
RicardoGoncalves-CS <126096527+RicardoGoncalves-CS@users.noreply.github.com>
2023-05-12 14:27:44 +0100

    LinkedListMerge exercise

---
+++ LinkedListMergeExercise/LinkedListMergeExercise/LinkedListMerge.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+
+namespace LinkedListMergeExercise;
+
+public static class LinkedListMerge
+{
+    public static LinkedList<int> MergeLists(LinkedList<int> list1, LinkedList<int> list2)
+    {
+        LinkedListNode<int> head1 = list1.First;
+        LinkedListNode<int> head2 = list2.First;
+
+        if (head1 == null)
+            return list2;
+
+        if (head2 == null)
+            return list1;
+
+        LinkedList<int> mergedList = new LinkedList<int>();
+        LinkedListNode<int> currentMerged;
+
+        if (head1.Value < head2.Value)
+        {
+            mergedList.AddLast(head1.Value);
+            currentMerged = mergedList.Last;
+            head1 = head1.Next;
+        }
+        else
+        {
+            mergedList.AddLast(head2.Value);
+            currentMerged = mergedList.Last;
+            head2 = head2.Next;
+        }
+
+        while (head1 != null && head2 != null)
+        {
+            if (head1.Value <= head2.Value)
+            {
+                mergedList.AddLast(head1.Value);
+                currentMerged = currentMerged.Next;
+                head1 = head1.Next;
+            }
+            else
+            {
+                mergedList.AddLast(head2.Value);
+                currentMerged = currentMerged.Next;
+                head2 = head2.Next;
+            }
+        }
+
+        while (head1 != null)
+        {
+            mergedList.AddLast(head1.Value);
+            currentMerged = currentMerged.Next;
+            head1 = head1.Next;
+        }
+
+        while (head2 != null)
+        {
+            mergedList.AddLast(head2.Value);
+            currentMerged = currentMerged.Next;
+            head2 = head2.Next;
+        }
+
+        return mergedList;
+    }
+}
---
+++ LinkedListMergeExercise/LinkedListMergeExercise/Program.cs
@@ -0,0 +1,21 @@
+﻿using LinkedListMergeExercise;
+using System.Collections.Generic;
+
+LinkedList<int> list1 = new LinkedList<int>();
+list1.AddLast(1);
+list1.AddLast(3);
+list1.AddLast(5);
+list1.AddLast(6);
+list1.AddLast(7);
+
+LinkedList<int> list2 = new LinkedList<int>();
+list2.AddLast(1);
+list2.AddLast(2);
+list2.AddLast(4);
+list2.AddLast(7);
+list2.AddLast(8);
+
+LinkedList<int> merged = LinkedListMerge.MergeLists(list1, list2);
+
+foreach(var node in merged)
+    Console.WriteLine(node);
